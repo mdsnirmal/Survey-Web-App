a885b76ffedab1290bd35e90c04ffc4a52d5dab4
Sachintha Nirmal <sachintha919@gmail.com>
2022-02-01 23:55:37 +0530

    Sign In page

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import store from './store'
+import './index.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).
+  use(store).
+  mount('#app')
--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,1 +1,16 @@
-const store  = createStore
+import {createStore} from "vuex";
+
+const store  = createStore({
+  state: {
+    user: {
+      data: { name : 'Zura'}, //to check whether my store state works without an issues
+      token: null
+    }
+  },
+  getters: {},
+  actions: {},
+  mutations: {},
+  modules: {}
+})
+export  default store;
+
